4ab4cc6583dc0b858b830564ae539edd474a5fb8
Tyler887 <tylermageeshields@gmail.com>
2022-01-22 15:46:11 +0000

    Create comment.rb

---
+++ app/models/comment.rb
@@ -0,0 +1,14 @@
+class Comment < ApplicationRecord
+  belongs_to :article
+end
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.string :commenter
+      t.text :body
+      t.references :article, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
