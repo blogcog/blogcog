7b9fd4f1625c7e5758bf6547e2775728a31a034b
Tyler887 <tylermageeshields@gmail.com>
2022-01-22 15:20:35 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
 end
+Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
+end
