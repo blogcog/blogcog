a70f72817a25f5d4ed9eb23cc6a448e6e96fa05a
Tyler887 <tylermageeshields@gmail.com>
2022-01-22 15:18:39 +0000

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,3 +12,10 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     end
   end
 end
+article = Article.new(title: "Your new blog has been created", body: "Hi! Welcome to your new blog. Run say article = Article.new(title: "Hello Rails", body: "I am on Rails!") on the command line to produce your first post. Then, try destroying this post.")
+time = today
+date.strftime('%d-%m-%Y')
+article.save
+article
+Article.find(1)
+Article.all
