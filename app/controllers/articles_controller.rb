c26e6c318bccb303ea9176b3fd911b5cc88b0175
Tyler887 <tylermageeshields@gmail.com>
2022-01-22 15:07:46 +0000

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,3 +2,13 @@ class ArticlesController < ApplicationController
   def index
   end
 end
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
